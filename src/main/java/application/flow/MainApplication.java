d3e3661032b617ed3b10a722fe353f34d72690ca
gasivelkay <gasivelkay@mtsbank.ru>
2024-07-21 16:12:21 +0300

    1 Каркас сервиса

---
+++ src/main/java/application/flow/MainApplication.java
@@ -0,0 +1,13 @@
+package application.flow;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication("ProcessApplication")
+public class MainApplication {
+    public static void main(final String... args) throws Exception {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
